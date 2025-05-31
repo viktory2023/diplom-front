6be799dcc9bb66eaab187dd71c7a7b450660a2f2
viktory2023 <vika.isaenko.2018@mail.ru>
2025-05-31 19:45:52 +0300

    auth

--- diplom/src/App.js
+++ diplom/src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import OrderPage from "./pages/OrderPage";
 import MakeOrderPage from "./pages/MakeOrderPage";
 import InfoPage from "./pages/InfoPage";
+import AuthPage from "./pages/AuthPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<OrderPage />} />
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/order" element={<OrderPage />} />
         <Route path="/order" element={<MakeOrderPage />} />
         <Route path="/info" element={<InfoPage />} />
       </Routes>
---
+++ diplom/src/pages/AuthPage.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import '../styles/AuthPage.css';
+
+export default function AuthPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Логин:', username);
+    console.log('Пароль:', password);
+  };
+
+  return (
+    <div className="login-container">
+      <div className="login-box">
+        <h2 className="login-title">Вход в систему</h2>
+        <input
+          type="text"
+          placeholder="Имя пользователя"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="login-input"
+        />
+        <input
+          type="password"
+          placeholder="Пароль"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="login-input"
+        />
+        <button onClick={handleLogin} className="login-button">Войти</button>
+        <p className="forgot-password">Забыли пароль?</p>
+      </div>
+    </div>
+  );
+}
